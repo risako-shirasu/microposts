cdd867bb7fb8b2923bb398afb42da5bbadfa7005
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-31 09:45:25 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  private
-
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
@@ -14,5 +12,7 @@ class ApplicationController < ActionController::Base
 
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
