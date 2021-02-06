307fcd01716c9e6186c60e1b16d37b93c50ab610
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-02-06 14:40:24 +0000

    edit_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes, :edit, :update]
+  #before_action :correct_user, only: [:edit, :update]
 
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -49,11 +50,20 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功したときの処理
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
   end
+  
 end
-
