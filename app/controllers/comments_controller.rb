21cf5ae28945ccbc5f2e81c15325fdaf10e6e484
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-02-14 12:17:24 +0000

    temporary_save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,12 @@ class CommentsController < ApplicationController
     @micropost = current_user.microposts.build
     @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     @comment = current_user.comments.build(comment_params)
-    @comment.micropost_id = params[:micropost_id]
+    #@comment.micropost_id = params[:micropost_id] 
     #@micropost.micropost_id = Micropost.find(params[:micropost_id])
     if @comment.save
       flash[:success] = "コメントしました"
       redirect_to root_path
+      #↑redirect_backを使うと不調気味なので_toを使用
     else
       flash[:success] = "コメントできませんでした"
       render 'toppages/index'
